3103f9c590e0533231b8ea86fbdfe704cc7cfd3e
William Zimmerman <will.zimmerman@betterment.com>
2025-08-10 16:35:16 +0100

    Start of major refactor - consolidating all instruction logic in one place; compiles but is instruction incomplete

--- haskell/src/Day5.hs
+++ haskell/src/Day5.hs
@@ -7,13 +7,13 @@ import Control.Monad.Error.Class (liftEither)
 import Control.Monad.State (MonadState (get), StateT, put, runStateT)
 import Data.List (uncons)
 import Data.List.Split (splitOn)
+import qualified Data.Map as M
 import Debug.Trace
 import qualified Opcode as O (
-    Instruction (Add, Equals, Halt, JumpIfFalse, JumpIfTrue, LessThan, Multiply, Read, Write),
     Opcode,
     ParameterMode (Immediate, Position),
     defaultMode,
-    instruction,
+    instructionCode,
     orderedParameterModes,
     parseOpcodeValue,
  )
@@ -24,17 +24,35 @@ type AddressableMemory = AL.AddressableList Int
 data Param = Immediate Int | Position Int deriving (Show)
 newtype InstructionPointer = MkInstructionPointer Int deriving (Show)
 
-data InstructionAndParams
-    = Add (Int, Int) AL.Address
-    | Multiply (Int, Int) AL.Address
+data Instruction
+    = Add Param Param Param
+    | Multiply Param Param Param
     | Halt
-    | Read AL.Address
-    | Write Int
-    | JumpIfTrue (Int, Int)
-    | JumpIfFalse (Int, Int)
-    | LessThan (Int, Int) AL.Address
-    | Equals (Int, Int) AL.Address
+    | Read Param
+    | Write Param
+    | JumpIfTrue Param Param
+    | JumpIfFalse Param
+    | LessThan Param Param Param
+    | Equals Param Param Param
     deriving (Show)
+
+data InstructionSpec = InstructionSpec
+    { instructionName :: String
+    , parameterCount :: Int
+    , builder :: [Param] -> Either ErrorMessage Instruction
+    }
+
+instructionTable :: M.Map Int InstructionSpec
+instructionTable =
+    M.fromList
+        [ (1, InstructionSpec "Add" 3 buildAdd)
+        ]
+
+type InstructionAndSpec = (Instruction, InstructionSpec)
+
+buildAdd :: [Param] -> Either ErrorMessage Instruction
+buildAdd [p1, p2, p3@(Position _)] = Right (Add p1 p2 p3)
+buildAdd xs = Left (printf "Unable to create Add instructions from parameters %s" (show xs))
 
 newtype StdIn = MkStdIn [Int] deriving (Show)
 newtype StdOut = MkStdOut [Int] deriving (Show)
@@ -49,24 +67,13 @@ read (MkStdIn (x : xs)) = (Just x, MkStdIn xs)
 write :: Int -> StdOut -> StdOut
 write x (MkStdOut xs) = MkStdOut (xs <> [x])
 
-numberOfParams :: O.Instruction -> Int
-numberOfParams O.Add = 3
-numberOfParams O.Multiply = 3
-numberOfParams O.Halt = 0
-numberOfParams O.Read = 1
-numberOfParams O.Write = 1
-numberOfParams O.JumpIfTrue = 2
-numberOfParams O.JumpIfFalse = 2
-numberOfParams O.LessThan = 3
-numberOfParams O.Equals = 3
-
 runProgram :: AppM
 runProgram = do
     (MkState instructionPointer memory _ stdOut) <- get
     opcode <- trace (show instructionPointer) liftEither (getOpcode instructionPointer memory)
-    instructionAndParams <- liftEither (getInstructionAndParams opcode instructionPointer memory)
-    case instructionAndParams of
-        Halt -> return stdOut
+    instructionAndSpec <- liftEither (getInstructionAndSpec opcode instructionPointer memory)
+    case instructionAndSpec of
+        (Halt, _) -> return stdOut
         actionableInstruction -> applyInstruction actionableInstruction >> runProgram
 
 start :: InstructionPointer
@@ -75,43 +82,19 @@ start = MkInstructionPointer 0
 getOpcode :: InstructionPointer -> AddressableMemory -> Either ErrorMessage O.Opcode
 getOpcode (MkInstructionPointer address) memory = AL.eitherLookup address memory >>= O.parseOpcodeValue
 
-getInstructionAndParams :: O.Opcode -> InstructionPointer -> AddressableMemory -> Either ErrorMessage InstructionAndParams
-getInstructionAndParams opcode instructionPointer memory = do
-    parameters <- getInstructionParams opcode instructionPointer memory
-    constructInstruction memory (O.instruction opcode) parameters
+getInstructionAndSpec :: O.Opcode -> InstructionPointer -> AddressableMemory -> Either ErrorMessage InstructionAndSpec
+getInstructionAndSpec opcode instructionPointer memory = do
+    instructionSpec <-
+        maybe
+            (Left $ printf "Encountered invalid instruction code %i" (O.instructionCode opcode))
+            Right
+            (M.lookup (O.instructionCode opcode) instructionTable)
+    parameters <- getInstructionParams instructionSpec (O.orderedParameterModes opcode) instructionPointer memory
+    instruction <- builder instructionSpec parameters
+    Right (instruction, instructionSpec)
 
-constructInstruction :: AddressableMemory -> O.Instruction -> [Param] -> Either ErrorMessage InstructionAndParams
-constructInstruction memory O.Add [p1, p2, Position address] = do
-    addend1 <- resolveParameter memory p1
-    addend2 <- resolveParameter memory p2
-    return (Add (addend1, addend2) address)
-constructInstruction memory O.Multiply [p1, p2, Position address] = do
-    multiplicand <- resolveParameter memory p1
-    multiplier <- resolveParameter memory p2
-    return (Multiply (multiplicand, multiplier) address)
-constructInstruction _ O.Halt [] = Right Halt
-constructInstruction _ O.Read [Position address] = Right (Read address)
-constructInstruction memory O.Write [p1] = Write <$> resolveParameter memory p1
-constructInstruction memory O.JumpIfTrue [p1, p2] = do
-    value1 <- resolveParameter memory p1
-    value2 <- resolveParameter memory p2
-    return (JumpIfTrue (value1, value2))
-constructInstruction memory O.JumpIfFalse [p1, p2] = do
-    value1 <- resolveParameter memory p1
-    value2 <- resolveParameter memory p2
-    return (JumpIfFalse (value1, value2))
-constructInstruction memory O.LessThan [p1, p2, Position address] = do
-    value1 <- resolveParameter memory p1
-    value2 <- resolveParameter memory p2
-    return (LessThan (value1, value2) address)
-constructInstruction memory O.Equals [p1, p2, Position address] = do
-    value1 <- resolveParameter memory p1
-    value2 <- resolveParameter memory p2
-    return (Equals (value1, value2) address)
-constructInstruction _ instruction params = Left (printf "Unable to parse parameters %s for instruction %s" (show params) (show instruction))
-
-getInstructionParams :: O.Opcode -> InstructionPointer -> AddressableMemory -> Either ErrorMessage [Param]
-getInstructionParams opcode (MkInstructionPointer address) memory =
+getInstructionParams :: InstructionSpec -> [O.ParameterMode] -> InstructionPointer -> AddressableMemory -> Either ErrorMessage [Param]
+getInstructionParams instructionSpec parameterModes (MkInstructionPointer address) memory =
     let
         getInstructionParams' :: Int -> [O.ParameterMode] -> AL.Address -> AddressableMemory -> [Either ErrorMessage Param]
         getInstructionParams' 0 _ _ _ = []
@@ -121,59 +104,24 @@ getInstructionParams opcode (MkInstructionPointer address) memory =
                 nextModes = maybe [] snd maybeHeadAndTail
              in fmap (translate currentMode) (AL.eitherLookup currentAddress memory') : getInstructionParams' (count - 1) nextModes (currentAddress + 1) memory'
      in
-        sequence $ getInstructionParams' (numberOfParams $ O.instruction opcode) (O.orderedParameterModes opcode) (address + 1) memory
+        sequence $ getInstructionParams' (parameterCount instructionSpec) parameterModes (address + 1) memory
 
 translate :: O.ParameterMode -> Int -> Param
 translate O.Position = Position
 translate O.Immediate = Immediate
 
-nextInstructionPointer :: O.Instruction -> InstructionPointer -> InstructionPointer
-nextInstructionPointer instruction (MkInstructionPointer address) =
-    MkInstructionPointer (address + numberOfParams instruction + 1)
+nextInstructionPointer :: InstructionSpec -> InstructionPointer -> InstructionPointer
+nextInstructionPointer instructionSpec (MkInstructionPointer address) =
+    MkInstructionPointer (address + parameterCount instructionSpec + 1)
 
-applyInstruction :: InstructionAndParams -> AppM
-applyInstruction (Add (addend1, addend2) outputAddress) = do
+applyInstruction :: InstructionAndSpec -> AppM
+applyInstruction (Add param1 param2 (Position outputAddress), instructionSpec) = do
     (MkState instructionPointer memory stdIn stdOut) <- get
-    put (MkState (nextInstructionPointer O.Add instructionPointer) (AL.replace outputAddress (addend1 + addend2) memory) stdIn stdOut)
+    addend1 <- liftEither $ resolveParameter memory param1
+    addend2 <- liftEither $ resolveParameter memory param2
+    put (MkState (nextInstructionPointer instructionSpec instructionPointer) (AL.replace outputAddress (addend1 + addend2) memory) stdIn stdOut)
     return stdOut
-applyInstruction (Multiply (multiplicand, multipler) outputAddress) = do
-    (MkState instructionPointer memory stdIn stdOut) <- get
-    put (MkState (nextInstructionPointer O.Multiply instructionPointer) (AL.replace outputAddress (multiplicand * multipler) memory) stdIn stdOut)
-    return stdOut
-applyInstruction Halt = do
-    (MkState _ _ _ stdOut) <- get
-    return stdOut
-applyInstruction (Read address) = do
-    (MkState instructionPointer memory stdIn stdOut) <- get
-    let (maybeReadValue, resultingStdIn) = Day5.read stdIn
-    let updatedMemory = maybe memory (\readValue -> AL.replace address readValue memory) maybeReadValue
-    put (MkState (nextInstructionPointer O.Read instructionPointer) updatedMemory resultingStdIn stdOut)
-    return stdOut
-applyInstruction (Write value) = do
-    (MkState instructionPointer memory stdIn stdOut) <- get
-    let newStdOut = write value stdOut
-    put (MkState (nextInstructionPointer O.Write instructionPointer) memory stdIn newStdOut)
-    return newStdOut
-applyInstruction (JumpIfTrue (v1, v2)) = do
-    (MkState instructionPointer memory stdIn stdOut) <- get
-    let newInstructionPointer = if v1 /= 0 then MkInstructionPointer v2 else nextInstructionPointer O.JumpIfTrue instructionPointer
-    put (MkState newInstructionPointer memory stdIn stdOut)
-    return stdOut
-applyInstruction (JumpIfFalse (v1, v2)) = do
-    (MkState instructionPointer memory stdIn stdOut) <- get
-    let newInstructionPointer = if v1 == 0 then MkInstructionPointer v2 else nextInstructionPointer O.JumpIfFalse instructionPointer
-    put (MkState newInstructionPointer memory stdIn stdOut)
-    return stdOut
-applyInstruction (LessThan (v1, v2) address) = do
-    (MkState instructionPointer memory stdIn stdOut) <- get
-    let valueToWrite = if v1 < v2 then 1 else 0
-    put (MkState (nextInstructionPointer O.LessThan instructionPointer) (AL.replace address valueToWrite memory) stdIn stdOut)
-    return stdOut
-applyInstruction (Equals (v1, v2) address) = do
-    (MkState instructionPointer memory stdIn stdOut) <- get
-    let valueToWrite = if v1 == v2 then 1 else 0
-    put (MkState (nextInstructionPointer O.Equals instructionPointer) (AL.replace address valueToWrite memory) stdIn stdOut)
-    return stdOut
+applyInstruction _ = liftEither (Left "Error!")
 
 resolveParameter :: AddressableMemory -> Param -> Either ErrorMessage Int
 resolveParameter _ (Day5.Immediate value) = Right value
--- haskell/src/Opcode.hs
+++ haskell/src/Opcode.hs
@@ -1,7 +1,6 @@
 module Opcode (
     parseOpcodeValue,
-    instruction,
-    Instruction (..),
+    instructionCode,
     Opcode,
     ParameterMode (..),
     orderedParameterModes,
@@ -11,9 +10,10 @@ module Opcode (
 import Data.Char (digitToInt)
 import Text.Printf (printf)
 
-data Opcode = MkOpcode Instruction [ParameterMode] deriving (Show)
+type InstructionCode = Int
 
-data Instruction = Add | Multiply | Halt | Read | Write | JumpIfTrue | JumpIfFalse | LessThan | Equals deriving (Show)
+data Opcode = MkOpcode InstructionCode [ParameterMode] deriving (Show)
+
 data ParameterMode = Position | Immediate deriving (Show)
 
 defaultMode :: ParameterMode
@@ -21,29 +21,16 @@ defaultMode = Position
 
 parseOpcodeValue :: Int -> Either String Opcode
 parseOpcodeValue value = do
-    let instructionCode = value `mod` 100
+    let instructionCode' = value `mod` 100
     let modeCodes = value `div` 100
-    instruction' <- parseInstructionCode instructionCode
     parameterModes <- parseModeCodes modeCodes
-    return (MkOpcode instruction' parameterModes)
+    return (MkOpcode instructionCode' parameterModes)
 
-instruction :: Opcode -> Instruction
-instruction (MkOpcode instruction' _) = instruction'
+instructionCode :: Opcode -> InstructionCode
+instructionCode (MkOpcode instructionCode' _) = instructionCode'
 
 orderedParameterModes :: Opcode -> [ParameterMode]
 orderedParameterModes (MkOpcode _ parameterModes) = parameterModes
-
-parseInstructionCode :: Int -> Either String Instruction
-parseInstructionCode 1 = Right Add
-parseInstructionCode 2 = Right Multiply
-parseInstructionCode 99 = Right Halt
-parseInstructionCode 3 = Right Read
-parseInstructionCode 4 = Right Write
-parseInstructionCode 5 = Right JumpIfTrue
-parseInstructionCode 6 = Right JumpIfFalse
-parseInstructionCode 7 = Right LessThan
-parseInstructionCode 8 = Right Equals
-parseInstructionCode value = Left (printf "Unable to parse instruction code %i" value)
 
 parseModeCodes :: Int -> Either String [ParameterMode]
 parseModeCodes value =
